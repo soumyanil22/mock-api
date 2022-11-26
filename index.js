1005c6aa58e061fd3bf7904a497e8a46999e91bf
soumya25 <soumyanil22@gmail.com>
2022-11-26 11:01:28 +0530

    added mock

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { exec } = require("child_process");
 // Set port (default: 3000). For Heroku, we need to use// the port set by the environment variable $PORTconst port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const command = `json-server --watch db.json --port ${port}`;
 exec(command, (err, stdout, stderr) => {
   if (err) {
